78b7ac0322d70650ea9439f9c77b37969a4eced4
md-amir-gauhar <gauharamir15@gmail.com>
2021-01-22 21:57:16 +0530

    useState with previous state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import HooksCounter from './components/useState/HooksCounter';
+import HooksCounterTwo from './components/useState/HooksCounterTwo';
 
 function App() {
   return (
     <div className="App">
       <HooksCounter />
+      <HooksCounterTwo />
     </div>
   );
 }
---
+++ src/components/useState/HooksCounterTwo.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+const HooksCounterTwo = () => {
+  const initialCount = 0;
+  const [count, setCount] = useState(initialCount);
+  return (
+    <div>
+      Count: {count}
+      <button onClick={() => setCount(initialCount)}>Reset</button>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Increment
+      </button>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)}>
+        Decrement
+      </button>
+    </div>
+  );
+};
+
+export default HooksCounterTwo;
