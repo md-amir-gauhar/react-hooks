fa3e4060cc69654bdf7e63dd9e1bd8b827a853e3
md-amir-gauhar <gauharamir15@gmail.com>
2021-01-22 22:14:23 +0530

    useState with object

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import HooksCounter from './components/useState/HooksCounter';
 import HooksCounterTwo from './components/useState/HooksCounterTwo';
+import HookCounterThree from './components/useState/HookCounterThree';
 
 function App() {
   return (
     <div className="App">
       <HooksCounter />
       <HooksCounterTwo />
+      <HookCounterThree />
     </div>
   );
 }
---
+++ src/components/useState/HookCounterThree.js
@@ -0,0 +1,27 @@
+//useState with object
+
+import React, { useState } from 'react';
+
+const HookCounterThree = () => {
+  const [name, setName] = useState({ firstName: '', lastName: '' });
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          value={name.firstName}
+          onChange={(e) => setName({ ...name, firstName: e.target.value })}
+        />
+        <input
+          type="text"
+          value={name.lastName}
+          onChange={(e) => setName({ ...name, lastName: e.target.value })}
+        />
+        <h3>Your first name is - {name.firstName}</h3>
+        <h3>Your last name is - {name.lastName}</h3>
+      </form>
+    </div>
+  );
+};
+
+export default HookCounterThree;
